4434263192e45ef71e4717d8e313059d90de52c3
Levente Bajczi <bajczilevi@gmail.com>
2017-11-19 15:41:22 +0100

    added Teilaufgabe 2 und 3+

---
+++ hazi2/src/hf_2.s
@@ -0,0 +1,31 @@
+DEF NACHLETZTE      0x28
+DEF BYTEMASK        0x0f
+DEF LD              0x80
+
+DATA
+
+SUM_LUT:
+    DB 0x00, 0x01, 0x01, 0x02, 0x01, 0x02, 0x02, 0x03, 0x01, 0x02, 0x02, 0x03, 0x02, 0x03, 0x03, 0x04
+    org 0x20
+DIGIT_CODE:
+    DB 0x29, 0x7B, 0x49, 0x56, 0x14, 0x39, 0x60
+
+CODE
+start:
+    MOV r0, #DIGIT_CODE ;Einladung von der Anfangsadresse
+    XOR r15, r15        ;Den Register, den wir als Zaehler benutzen werden, ausnullen
+loop:
+    MOV r1, (r0)        ;Dateneinladung
+    AND r1, #BYTEMASK   ;untere 4 bits ausmasken
+    MOV r14, (r1)       ;basisadresse ist 0, also die Offset allein koennte als Adresse benutzt werden
+    ADD r15, r14
+    MOV r1, (r0)        ;Dateneinladung noch einmal
+    SWP r1              ;Untere und obene Bits austauschen
+    AND r1, #BYTEMASK   ;untere 4 bits ausmasken
+    MOV r14, (r1)       ;basisadresse ist 0, also die Offset allein koennte als Adresse benutzt werden
+    ADD r15, r14
+    ADD r0, #0x01       ;Adresse inkrementieren
+    CMP r0, #NACHLETZTE ;Falls sie die gleiche Wert betragen, sind wir fertig
+    JNZ loop
+endloop:
+    MOV LD, r15         ;Ergebnis auf der LEDs darstellen
---
+++ hazi2/src/hf_3.s
@@ -0,0 +1,41 @@
+DEF LD          0x80                ; LED adatregiszter (írható/olvasható)
+DEF GERADE1     0b01010101          ;MASKE
+DEF GERADE2     0b00110011          ;
+DEF GERADE4     0b00001111          ;
+DEF NACHLETZTE  0x08                ;Erste adresse, die wir nicht benutzen
+
+DATA
+DIGIT_CODE:
+    DB 0x29, 0x7B, 0x49, 0x56, 0x14, 0x39, 0x60
+
+
+CODE
+start:
+    MOV r0, #DIGIT_CODE     ;Addresse einlesen
+    XOR r15, r15            ;Ergebnisregister ausnullen
+    
+loop:
+    MOV r1, (r0)            ;Erste Byte einlesen
+    MOV r2, r1              ;Kopieren
+    AND r1, #GERADE1        ;Gerade Bits ausmasken (0-te, 2-te, 4-te, 6-te)
+    SR0 r2                  ;Rechts verschieben, um gleiche Maske benutzen zu koennen
+    AND r2, #GERADE1        ;jetzt gerade Bits ausmasken (vorherig 1-te, 3-te, 5-te, 7-te)
+    ADD r1, r2              ;Zusammensummieren
+    MOV r2, r1              ;Kopieren
+    AND r1, #GERADE2        ;Gerade Zweibits ausmasken (0-1, 4-5)
+    SR0 r2
+    SR0 r2                  ;Zwei Stellen nach rechts verschieben
+    AND r2, #GERADE2        ;jetzt gerade Zweibits ausmasken (vorherig 2-3, 6-7)
+    ADD r1, r2              ;Summieren
+    MOV r2, r1              ;Kopieren
+    AND r1, #GERADE4        ;Untere 4 bits ausmasken
+    SWP r2                  ;Obene und untere Bits austauschen
+    AND r2, #GERADE4        ;jetzt untere 4 bits ausmasken (vorherig obene 4)
+    ADD r1, r2              ;Zusammensummieren
+    ADD r15, r1             ;Ergebnis speichern
+    ADD r0, #1              ;Addressenzeiger erhöhen
+    CMP r0, #NACHLETZTE     ;Falls wir fertig sind, sind sie gleich
+    JNZ loop
+    
+endloop:
+    MOV LD, r15             ;Ergebnis auf die LEDs darstellen
